43e0e26a0f22f8f89dde41b0ccc695f960fd0b21
AudeNec <aude.nec@gmail.com>
2024-10-21 16:31:38 +0200

    quest 5

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const pokemonList = [
 function App() {
   return (
     <section>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
     </section>
   );
 }
