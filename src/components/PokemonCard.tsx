ae4abf932ecd09da52e22e65e090b691364b2773
AudeNec <aude.nec@gmail.com>
2024-10-21 15:12:04 +0200

    quest 2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,26 @@
 function PokemonCard() {
+  const pokemon = pokemonList[0];
   return (
     <figure>
-      <img
-        alt="Bulbasaur"
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc !== undefined ? (
+        <img alt={pokemon.name} src={pokemon.imgSrc} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
