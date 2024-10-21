7b81e608fb4ed7bb53a16a75c6312be7af5f72b7
AudeNec <aude.nec@gmail.com>
2024-10-21 14:56:05 +0200

    quest 2

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,13 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img
+        alt="Bulbasaur"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+      />
+      <figcaption>Bulbasaur</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
