552ad842e3f8c5d4705d47c28a80047ebb8efee9
AudeNec <aude.nec@gmail.com>
2024-10-23 10:02:50 +0200

    quete 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,43 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import PokemonCard from "./components/PokemonCard.tsx";
 import NavBar from "./components/NavBar.tsx";
 
 function App() {
+  useEffect(() => {
+    alert("Hello Pokemon trainer :) ");
+  }, []);
+
   const pokemonList = [
     {
-      name: "bulbasaur",
+      name: "Bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      name: "charmander",
+      name: "Charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
     },
     {
-      name: "squirtle",
+      name: "Squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
     },
     {
-      name: "pikachu",
+      name: "Pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
-      name: "mew",
+      name: "Mew",
     },
   ];
 
   const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  if (pokemonIndex === 3) alert("pika pikachu !!!");
 
   return (
     <section>
